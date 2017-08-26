8850a20399fd458bdb33f43e29aee5fc075732cf
Rinat Khabibiev <srenskiy@gmail.com>
2017-08-26 14:48:12 +0300

    new version: 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as description:
 
 setup(
     name='django-bitmask-field',
-    version='0.1',
+    version='0.1.1',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     py_modules=['django_bitmask_field'],
